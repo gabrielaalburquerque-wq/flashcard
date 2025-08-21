b2842a9c4d9ca93d43c7fca96fc3b097e97fdccf
gabrielaalburquerque-wq <gabriela.alburquerque@escola.pr.gov.br>
2025-08-21 11:20:03 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'O que é matemática?',
+    'A matemática é uma linguagem de ciência que estuda padrões'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
